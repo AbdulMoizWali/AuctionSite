6395792063d1475501c42c28b24ce2bec754b53a
AbdulMoizWali <moisabdul47@gmail.com>
2022-12-17 02:07:26 +0500

    Created Webservices for Auction and Product - Insert Auction With Product - Update Auction - Insert Product etc...

---
+++ ProjectWorkspace/AuctionSite/src/main/Auction.java
@@ -0,0 +1,155 @@
+package main;
+import java.util.Date;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+public class Auction {
+
+	public static void main(String[] args) {
+		Auction auction = new Auction(); 
+		auction.InsertAuctionWithProduct("Probduct6", "Descrdgbgipdgntion", "Catsbdgegory", "Locatbvifgon", "2022-12-16 15:16:18", "10000");
+		auction.getAuctions();
+	}
+
+	
+	public String getProductAuctionInformation(String ProductName) {
+		String result = "";
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/AuctionSite","root","");
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("Select `auctioned product`.ProductID, `auctioned product`.`AuctionID`, `auctioned product`.`Start`, `auctioned product`.`Ended`, `auctioned product`.`minBidPrice`, `Status`, `Approval`, `product`.`Title`, `product`.`Description`, `product`.`Category`, `product`.`Location` FROM `auctioned product`, `product` where `product`.`Title` = '" + ProductName + "';");
+			rs.next();
+			result = " ProductID: " + rs.getInt(1) +
+					" AuctionID: " + rs.getInt(2) + 
+					" Start: " + rs.getDate(3).toString() + 
+					" End: " + rs.getDate(4).toString() + 
+					" MinBidPrice: " + rs.getString(5) + 
+					" Status: " + rs.getString(6) +
+					" Approval: " + rs.getString(7) +
+					" Title: " + rs.getString(8) +
+					" Description: " + rs.getString(9) +
+					" Category: " + rs.getString(10) +
+					" Location: " + rs.getString(11)
+					;
+			System.out.println(result);
+			con.close();
+		} catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return result;
+	}
+	
+	public Date addHoursToJavaUtilDate(Date date, int hours) {
+	    Calendar calendar = Calendar.getInstance();
+	    calendar.setTime(date);
+	    calendar.add(Calendar.HOUR_OF_DAY, hours);
+	    return (Date) calendar.getTime();
+	}
+	
+	public void InsertAuctionWithProduct(String Title, String Description, String Category, String Location, String Start, String MinBidPrice) {
+		if(Integer.parseInt(MinBidPrice) < 5000) {
+			MinBidPrice = "5000";
+		}
+		Product product = new Product();
+		product.InsertProduct(Title, Description, Category, Location);
+		
+		if(product.getProductID() != 0) {
+			InsertAuction(product.getProductID() + "", Start, MinBidPrice);							
+		}
+	}
+	
+	public void InsertAuction(String ProductID, String StartDate, String MinBidPrice) {
+		if(Integer.parseInt(MinBidPrice) < 5000) {
+			MinBidPrice = "5000";
+		}
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
+	    Date Start = null;
+		try {
+			Start = formatter.parse(StartDate);
+		} catch (ParseException e1) {
+			System.out.println(e1);
+		} 
+		Date End = addHoursToJavaUtilDate(Start, 24);
+		String EndDate = formatter.format(End);
+		String query = "INSERT INTO `auctioned product` (`ProductID`, `Start`, `Ended`, `minBidPrice`, `Approval`) VALUES ('" + ProductID + "', '" + StartDate + "', '" + EndDate + "', '" + MinBidPrice + "', 'False' )";
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/AuctionSite","root","");
+			Statement stmt=con.createStatement();
+			int index = stmt.executeUpdate(query);
+			if(index > 0) {
+				System.out.println("Auction Data Inserted");
+			}
+			else {
+				System.out.println("Auction Failed Insertion");
+			}
+			con.close();
+		} catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+	
+	public void UpdateAuctionStatus(String AuctionID, String Status, boolean Approved) {
+		String Updatequery = "UPDATE `auctioned product` SET `Status` = '" + Status + "', `Approval` = '" + Approved + "' WHERE `auctioned product`.`AuctionID` = " + AuctionID;
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/AuctionSite","root","");
+			Statement stmt=con.createStatement();
+			int index = stmt.executeUpdate(Updatequery);
+			if(index > 0) {
+				System.out.println("Data Updated");
+			}
+			else {
+				System.out.println("Failed Updation");
+			}
+			con.close();
+		} catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+	public String getAuctions() {
+		String result = "";
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/AuctionSite","root","");
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("SELECT * FROM `auctioned product`;");
+
+			while(rs.next())
+			{
+			result += 
+					" AuctionID: " + rs.getInt(1) +     
+					" ProductID: " + rs.getInt(2) +
+					" Start: " + rs.getDate(3).toString() + 
+					" End: " + rs.getDate(4).toString() + 
+					" MinBidPrice: " + rs.getString(5) + 
+					" Status: " + rs.getString(6) +
+					" Approval: " + rs.getString(7) + "\n"
+					;
+			}
+			System.out.println(result);
+			con.close();
+		} catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return result;
+	}
+
+}
---
+++ ProjectWorkspace/AuctionSite/src/main/Product.java
@@ -0,0 +1,115 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Product {
+	private int ProductID = 0;
+	private String Title;
+	private String Description; 
+	private String Category;
+	private String Location;
+	
+	
+	/*public static void main(String[] args) {
+
+		Product product = new Product();
+		product.InsertProduct("Title", "Description", "Category", "Location");
+		product.getProducts();
+	}*/
+	
+
+	
+	public void InsertProduct(String Title, String Description, String Category, String Location) {
+		this.Title = Title;
+		this.Description = Description;
+		this.Category = Category;
+		this.Location = Location;
+		setLatestProductID();
+		if(ProductID != 0) {
+			return;
+		}
+		//InsertProduct(Title, Description, Category, Location);			
+		String Insertquery = "INSERT INTO `product` (`Title`, `Description`, `Category`, `Location`) VALUES ('" + Title + "', '" + Description + "', '" + Category + "', '" + Location + "' )";
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/AuctionSite","root","");
+			Statement stmt=con.createStatement();
+			int index = stmt.executeUpdate(Insertquery);
+			if(index > 0) {
+				System.out.println("Product Data Inserted");
+				setLatestProductID();
+			}
+			else {
+				System.out.println("Product Failed Insertion");
+			}
+			con.close();
+		} catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+	
+	
+	public void setLatestProductID() {
+		String ProductSearchquery = "SELECT * FROM `product` WHERE `Title` = '" + this.Title + "' AND `Description` = '"+this.Description+"' AND `Category` = '"+this.Category+"' AND `Location` = '"+this.Location+"'";
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/AuctionSite","root","");
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery(ProductSearchquery);
+			//System.out.println(ProductSearchquery);
+			if(rs != null) {
+				rs.next();
+				try {
+					ProductID = rs.getInt(1);
+				}
+				catch (Exception e2){
+					ProductID = 0;
+				}
+			}
+			con.close();
+		} catch(Exception e)
+		{
+			System.out.println(e + " setLatestProductID()");
+		}
+	} 
+	
+	
+	public int getProductID() {
+		return ProductID;
+	}
+	
+	public String getProducts() {
+		String result = "";
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/AuctionSite","root","");
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("SELECT * FROM `product`");
+
+			while(rs.next())
+			{
+			result += 
+					" ProductID: " + rs.getInt(1) +
+					" Title: " + rs.getString(2) +
+					" Description: " + rs.getString(3) +
+					" Category: " + rs.getString(4) +
+					" Location: " + rs.getString(5) + "\n"
+					;
+			}
+			System.out.println(result);
+			con.close();
+		} catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return result;
+	}
+}
