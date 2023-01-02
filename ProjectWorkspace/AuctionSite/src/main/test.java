0ef769cfb95fe7501c0b2cac7c9aeb695f9624d1
siddiquegulsher <salmangulsher@gmail.com>
2023-01-02 17:34:54 +0500

    User web services done

---
+++ ProjectWorkspace/AuctionSite/src/main/test.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.sql.*;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		User user1 = new User();
+		Auction aut1 = new Auction();
+//		aut1.getProductAuctionInformation("Produc22");
+//		user1.insertUserData("Amoiz", "12345","Abdul", "Moiz", "4264326326", "karachi", "512351235215");
+//user1.getUserData("3");
+//		System.out.println(user1.getloginID("A_moiz", "54321"));
+		user1.getProductUserData(2);
+//		user1.updateUsernamePass("3", "A_moiz", "54321");
+	}
+
+}
